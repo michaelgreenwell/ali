86924be5c50b97654f8210e7e5cff0c0ed000d58
Michael Greenwell <michael.greenwell@moz.com>
2019-01-30 23:31:44 -0800

    This makes the initial path an argument and looks for all of the files

--- extract.py
+++ extract.py
@@ -3,20 +3,18 @@ import re
 import xml.etree.ElementTree
 import zipfile
 import csv
+import sys
 
-ROOT = "/Users/michael/ali/data"
-
-# This will look for zip files that contain XML
-def find_zip_files():
+# This will look for files paths under the provided root directory that match the provided regular expression recursively
+def find_files_by_regex(root, regex):
   zip_files = []
-  for path, _, files in os.walk(ROOT):
+  for path, _, files in os.walk(root):
     for file in files:
       filepath = path + "/" + file
-      if re.compile('(.*zip$)').match(filepath):
+      if re.compile(regex).match(filepath):
         print filepath
         zip_files.append(filepath)
   return zip_files
-
 
 # This will open those zip files and pull out the XML
 def read_zip_file_xml(filepath):
@@ -29,34 +27,46 @@ def read_zip_file_xml(filepath):
       xml_strings.append(content)
   return xml_strings
 
+# This is used to wrap fields that may not appear in the XML
+def text_or_none(find_result):
+  return None if find_result is None else find_result.text
+
 # This will read an XML file in with the expected format and create a Dictionary
 def dict_from_xml_string(xml_string):
   record = xml.etree.ElementTree.fromstring(xml_string)
   publication = record.find('Publication')
+
   contributor = record.find('Contributor')
-  return {
-    'record_id': record.find('RecordID').text,
-    'record_title': record.find('RecordID').text,
-    'publication_id': publication.find('PublicationID').text,
-    'publication_title': publication.find('Title').text,
-    'publication_qualifier': publication.find('Qualifier').text,
-    'publisher': record.find('Publisher').text,
-    'volume': record.find('Volume').text,
-    'issue': record.find('Issue').text,
-    'alpha_pub_date': record.find('AlphaPubDate').text,
-    'numeric_pub_date': record.find('NumericPubDate').text,
-    'source_type': record.find('SourceType').text,
-    'object_type': record.find('ObjectType').text,
-    'contributor_role': contributor.find('ContribRole').text,
-    'contributor_last_name': contributor.find('LastName').text,
-    'contributor_first_name': contributor.find('FirstName').text,
-    'contributor_person_name': contributor.find('PersonName').text,
-    'contributor_original_form': contributor.find('OriginalForm').text,
-    'start_page': record.find('StartPage').text,
-    'end_page': record.find('EndPage').text,
-    'pagination': record.find('Pagination').text,
-    'url_doc_view': record.find('URLDocView').text,
+  contributor_dict = {}
+  if contributor is not None:
+    contributor_dict = {
+      'contributor_role': text_or_none(contributor.find('ContribRole')),
+      'contributor_last_name': text_or_none(contributor.find('LastName')),
+      'contributor_first_name': text_or_none(contributor.find('FirstName')),
+      'contributor_person_name': text_or_none(contributor.find('PersonName')),
+      'contributor_original_form': text_or_none(contributor.find('OriginalForm')),
+    }
+
+  return_dict = {
+    'record_id': text_or_none(record.find('RecordID')),
+    'record_title': text_or_none(record.find('RecordID')),
+    'publication_id': text_or_none(publication.find('PublicationID')),
+    'publication_title': text_or_none(publication.find('Title')),
+    'publication_qualifier': text_or_none(publication.find('Qualifier')),
+    'publisher': text_or_none(record.find('Publisher')),
+    'volume': text_or_none(record.find('Volume')),
+    'issue': text_or_none(record.find('Issue')),
+    'alpha_pub_date': text_or_none(record.find('AlphaPubDate')),
+    'numeric_pub_date': text_or_none(record.find('NumericPubDate')),
+    'source_type': text_or_none(record.find('SourceType')),
+    'object_type': text_or_none(record.find('ObjectType')),
+    'start_page': text_or_none(record.find('StartPage')),
+    'end_page': text_or_none(record.find('EndPage')),
+    'pagination': text_or_none(record.find('Pagination')),
+    'url_doc_view': text_or_none(record.find('URLDocView')),
   }
+  return_dict.update(contributor_dict)
+  return return_dict
 
 def write_dict_array_to_csv(dict_array):
   with open('output.csv', 'wb') as output_file:
@@ -64,10 +74,21 @@ def write_dict_array_to_csv(dict_array):
     dict_writer.writeheader()
     dict_writer.writerows(dict_array)
 
-zip_files = find_zip_files()
-xml_strings = read_zip_file_xml(zip_files.pop(0))
-row = dict_from_xml_string(xml_strings.pop(0))
-write_dict_array_to_csv([row])
+
+root = sys.argv[1]
+XML_FILE_REGEX = '.*/(xml|XML)/(.*zip$)' # This regular expression identifies file paths for XML zips
+
+zip_files = find_files_by_regex(root, XML_FILE_REGEX)
+
+xml_strings = []
+for zip_file in zip_files:
+  xml_strings = xml_strings + read_zip_file_xml(zip_files.pop(0))
+
+rows = []
+for xml_string in xml_strings:
+  rows.append(dict_from_xml_string(xml_strings.pop(0)))
+
+write_dict_array_to_csv(rows)
 
 
 
