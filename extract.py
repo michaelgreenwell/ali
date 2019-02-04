4cf7ef3848ace2094fbed13c160ccbbddb02f1ea
Michael Greenwell <michael.greenwell@moz.com>
2019-02-03 20:31:11 -0800

    This will store each row in an OrderedDict so the columns come out the same

--- extract.py
+++ extract.py
@@ -6,6 +6,32 @@ import csv
 import sys
 import copy
 import codecs
+import collections
+
+# This is the template for each row
+ROW_TEMPLATE = collections.OrderedDict({
+  'publication_id': None,
+  'publication_title': None,
+  'publication_qualifier': None,
+  'contributor_role': None,
+  'contributor_last_name': None,
+  'contributor_first_name': None,
+  'contributor_person_name': None,
+  'contributor_original_form': None,
+  'record_id': None,
+  'record_title': None,
+  'publisher': None,
+  'volume': None,
+  'issue': None,
+  'alpha_pub_date': None,
+  'numeric_pub_date': None,
+  'source_type': None,
+  'object_type': None,
+  'start_page': None,
+  'end_page': None,
+  'pagination': None,
+  'url_doc_view': None
+})
 
 # This will look for files paths under the provided root directory that match the provided regular expression recursively
 def find_files_by_regex(root, regex):
@@ -48,28 +74,27 @@ def text_or_none(find_result):
 # This will read an XML file in with the expected format and create a Dictionary
 def dicts_from_xml_string(xml_string, pdf_paths):
   record = xml.etree.ElementTree.fromstring(xml_string)
+  main_dict = copy.deepcopy(ROW_TEMPLATE)
 
   publication = record.find('Publication')
-  publication_dict = {}
   if publication is not None:
-    publication_dict = {
+    main_dict.update({
       'publication_id': text_or_none(publication.find('PublicationID')),
       'publication_title': text_or_none(publication.find('Title')),
       'publication_qualifier': text_or_none(publication.find('Qualifier')),
-    }
+    })
 
   contributor = record.find('Contributor')
-  contributor_dict = {}
   if contributor is not None:
-    contributor_dict = {
+    main_dict.update({
       'contributor_role': text_or_none(contributor.find('ContribRole')),
       'contributor_last_name': text_or_none(contributor.find('LastName')),
       'contributor_first_name': text_or_none(contributor.find('FirstName')),
       'contributor_person_name': text_or_none(contributor.find('PersonName')),
       'contributor_original_form': text_or_none(contributor.find('OriginalForm')),
-    }
+    })
 
-  main_dict = {
+  main_dict.update({
     'record_id': text_or_none(record.find('RecordID')),
     'record_title': text_or_none(record.find('RecordID')),
     'publisher': text_or_none(record.find('Publisher')),
@@ -83,13 +108,9 @@ def dicts_from_xml_string(xml_string, pdf_paths):
     'end_page': text_or_none(record.find('EndPage')),
     'pagination': text_or_none(record.find('Pagination')),
     'url_doc_view': text_or_none(record.find('URLDocView')),
-  }
+  })
 
-  main_dict.update(publication_dict)
-  main_dict.update(contributor_dict)
-
-
-
+  # This is a sub function of dicts_from_xml_string that duplicates the record for each Product
   def merge_product_id(product_element):
     md = copy.deepcopy(main_dict)
     md.update({'product_id': text_or_none(product_element.find('ProductID'))})
@@ -101,14 +122,13 @@ def dicts_from_xml_string(xml_string, pdf_paths):
   products = map(merge_product_id, product_elements)
 
   for product in products:
-    pdf_path = pdf_paths.get('file_name', None)
-    if pdf_path is not None:
-      product.update(pdf_path)
-    else:
-      product.update({
-        'zipfile_path': None,
-        'zipped_pdf_path': None
-      })
+    pdf_path = {
+      'zipfile_path': None,
+      'zipped_pdf_path': None
+    }
+    pdf_path.update(pdf_paths.get('file_name', {}))
+    product.update(pdf_path)
+
   return products
 
 ROOT = sys.argv[1]
@@ -122,6 +142,7 @@ for zip_file in pdf_zip_files:
 
 with codecs.open('output.csv', 'w', 'utf-8') as fp:
   writer = csv.writer(fp)
+  writer.writerow(ROW_TEMPLATE.keys())
   xml_zip_files = find_files_by_regex(ROOT, XML_ZIP_FILE_REGEX)
   for zip_file in xml_zip_files:
     xml_strings = read_xml_zip_file(zip_file)
